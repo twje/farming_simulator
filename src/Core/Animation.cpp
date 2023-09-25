b5bd7992638dd72e1c536a70279b3b52368fa810
twje <11610634+twje@users.noreply.github.com>
2023-09-25 23:33:01 +1000

    refactor Animation class

--- src/Core/Animation.cpp
+++ src/Core/Animation.cpp
@@ -1,6 +1,123 @@
 #include "Core/Animation.h"
+
+#include <cassert>
+#include <iostream>
+#include <fstream>
+
 #include "Core/TextureAnimationSequence.h"
 #include "Core/ResourceLocator.h"
+
+// ----------------------------------------------------------
+sf::Vector2f SequenceFrame::GetOrigin(const sf::Vector2f& originAnchor)
+{
+	float originX = originAnchor.x * mTextureRect.getSize().x;
+	float originY = originAnchor.y * mTextureRect.getSize().y;
+	return sf::Vector2f(originX, originY);
+}
+
+// ----------------------------------------------------------
+AnimationSequence::AnimationSequence(std::string sequenceId, uint16_t framesPerSecond)
+	: mSequenceId(sequenceId),
+	mFramesPerSecond(framesPerSecond),
+	mDuration(1.0f / framesPerSecond)
+{ }
+
+
+// ----------------------------------------------------------
+Animation::Animation(const Animation& other)
+{
+	for (const auto& sequencePair : other.mSequences)
+	{
+		AddAnimationSequence(sequencePair.second);
+	}
+}
+
+void Animation::Upate(const sf::Time& timestamp)
+{
+	assert(mCurrentSequence != nullptr);
+	mTimer.Update(timestamp);
+	if (mTimer.IsFinished())
+	{
+		mFrameIndex = (mFrameIndex + 1) % mCurrentSequence->GetFrameCount();
+		RefreshFrame();
+		mTimer.Reset();
+	}
+}
+
+sf::Sprite& Animation::GetSprite()
+{ 
+	assert(mSprite != nullptr);
+	return *mSprite; 
+}
+
+sf::FloatRect Animation::GetGlobalBounds() const
+{
+	assert(mSprite != nullptr);
+	return mSprite->getGlobalBounds();
+}
+
+void Animation::SetOriginAnchor(sf::Vector2f originAnchor)
+{
+	mOriginAnchor = originAnchor;
+	auto& frame = GetFrame();
+	mSprite->setOrigin(frame.GetOrigin(mOriginAnchor));
+}
+
+void Animation::AddAnimationSequence(std::shared_ptr<AnimationSequence> sequence)
+{
+	const auto& sequenceId = sequence->GetSequenceId();
+	assert(mSequences.find(sequenceId) == mSequences.end());
+
+	mSequences[sequenceId] = sequence;
+	if (mCurrentSequence == nullptr)
+	{
+		SetAnimationSequence(sequenceId);
+	}
+}
+
+void Animation::SetAnimationSequence(const std::string& sequenceId)
+{
+	if (mCurrentSequence != nullptr && mCurrentSequence->GetSequenceId() == sequenceId)
+	{
+		return;
+	}
+
+	auto iter = mSequences.find(sequenceId);
+	assert(iter != mSequences.end());
+	mCurrentSequence = iter->second.get();
+	mTimer.Reset(mCurrentSequence->GetDuration());
+	mFrameIndex = 0;
+	RefreshFrame();
+}
+
+void Animation::SaveToFile(const std::string& filePath)
+{
+	YAML::Emitter emitter;
+	Serialize(emitter);
+	std::ofstream file(filePath);
+	file << emitter.c_str();
+}
+
+void Animation::LoadFromFile(const std::string& filePath, ResourceLocator& locator)
+{
+	YAML::Node node = YAML::LoadFile(filePath);
+	Deserialize(node, locator);
+}
+
+void Animation::Serialize(YAML::Emitter& emitter)
+{
+	emitter << YAML::BeginSeq;
+	for (const auto& sequence : mSequences)
+	{
+		emitter << YAML::BeginMap;
+		emitter << YAML::Key << "key" << YAML::Value << sequence.first;
+		emitter << YAML::Key << "framesPerSecond" << YAML::Value << sequence.second->GetFramesPerSecond();
+		emitter << YAML::Key << "frames" << YAML::Value;
+		sequence.second->Serialize(emitter);
+		emitter << YAML::EndMap;
+	}
+	emitter << YAML::EndSeq;
+}
 
 void Animation::Deserialize(const YAML::Node& node, ResourceLocator& locator)
 {
@@ -33,7 +150,7 @@ void Animation::Deserialize(const YAML::Node& node, ResourceLocator& locator)
 	// Iterate over nodes in the input YAML
 	for (YAML::const_iterator it = node.begin(); it != node.end(); ++it)
 	{
-		std::string sequenceId = (*it)["key"].as<std::string>();
+		std::string sequenceId = (*it)["sequenceId"].as<std::string>();
 		uint16_t framesPerSecond = (*it)["framesPerSecond"].as<uint16_t>();
 		for (const auto& frames : (*it)["frames"])
 		{
@@ -47,4 +164,26 @@ void Animation::Deserialize(const YAML::Node& node, ResourceLocator& locator)
 			AddAnimationSequence(sequence);
 		}
 	}
+}
+
+void Animation::RefreshFrame()
+{
+	auto& frame = GetFrame();
+	if (mSprite == nullptr)
+	{
+		mSprite = std::make_unique<sf::Sprite>(*frame.mTexture);
+	}
+	else
+	{
+		mSprite->setTexture(*frame.mTexture);
+	}
+	mSprite->setTextureRect(frame.mTextureRect);
+	mSprite->setOrigin(frame.GetOrigin(mOriginAnchor));
+}
+
+SequenceFrame& Animation::GetFrame()
+{
+	assert(mCurrentSequence != nullptr);
+	mCurrentSequence->GetFrame(mOutSequenceFrame, mFrameIndex);
+	return mOutSequenceFrame;
 }
--- src/Core/Animation.h
+++ src/Core/Animation.h
@@ -1,13 +1,9 @@
 #pragma once
 
-#include <map>
-#include <cassert>
-#include <iostream>
-#include <fstream>
+#include <unordered_map>
 
 #include <SFML/Graphics.hpp>
 
-#include "Core/GameObject.h"
 #include "Core/Timer.h"
 #include "Core/ISerializable.h"
 
@@ -16,25 +12,16 @@ class TextureManager;
 
 struct SequenceFrame
 {	
+	sf::Vector2f GetOrigin(const sf::Vector2f& originAnchor);
+	
 	sf::Texture* mTexture{ nullptr };
-	sf::IntRect mTextureRect;
-
-	sf::Vector2f GetOrigin(const sf::Vector2f& originAnchor)
-	{
-		float originX = originAnchor.x * mTextureRect.getSize().x;
-		float originY = originAnchor.y * mTextureRect.getSize().y;
-		return sf::Vector2f(originX, originY);
-	}
+	sf::IntRect mTextureRect;	
 };
 
 class AnimationSequence : public ISerializable
 {
 public:
-	AnimationSequence(std::string sequenceId, uint16_t framesPerSecond)
-		: mSequenceId(sequenceId),		
-		  mFramesPerSecond(framesPerSecond),
-		  mDuration(1.0f / framesPerSecond)
-	{ }
+	AnimationSequence(std::string sequenceId, uint16_t framesPerSecond);
 	
 	// Getters
 	const std::string& GetSequenceId() { return mSequenceId; }	
@@ -55,133 +42,34 @@ class Animation : public ISerializable
 {
 public:	
 	Animation() = default;
-
-	Animation(const Animation& other)		
-	{
-		for (const auto& sequencePair : other.mSequences)
-		{
-			AddAnimationSequence(sequencePair.second);
-		}
-	}
-
-	void Upate(const sf::Time& timestamp)
-	{
-		assert(mCurrentSequence != nullptr);
-		mTimer.Update(timestamp);
-		if (mTimer.IsFinished())
-		{
-			mFrameIndex = (mFrameIndex + 1) % mCurrentSequence->GetFrameCount();	
-			UpdateFrame();
-			mTimer.Reset();
-		}
-	}
+	Animation(const Animation& other);
+	
+	void Upate(const sf::Time& timestamp);
 
 	// Getters
-	sf::Sprite& GetSprite() { return *mSprite; }
-
-	sf::FloatRect GetGlobalBounds() const
-	{ 
-		assert(mSprite != nullptr);
-		return mSprite->getGlobalBounds(); 
-	}
+	sf::Sprite& GetSprite();
+	sf::FloatRect GetGlobalBounds() const;
 
 	// Setters
-	void SetOriginAnchor(sf::Vector2f originAnchor) 
-	{ 
-		mOriginAnchor = originAnchor;
-		auto& frame = GetFrame();
-		mSprite->setOrigin(frame.GetOrigin(mOriginAnchor));
-	}
+	void SetOriginAnchor(sf::Vector2f originAnchor);
+	void AddAnimationSequence(std::shared_ptr<AnimationSequence> sequence);
+	void SetAnimationSequence(const std::string& sequenceId);
 
-	void AddAnimationSequence(std::shared_ptr<AnimationSequence> sequence)
-	{		
-		const auto& sequenceId = sequence->GetSequenceId();
-		assert(mSequences.find(sequenceId) == mSequences.end());
-		
-		mSequences[sequenceId] = sequence;
-		if (mCurrentSequence == nullptr)
-		{
-			Initialize(sequenceId);
-		}
-	}
-
-	void SetAnimationSequence(const std::string& sequenceId)
-	{		
-		if (mCurrentSequence != nullptr && mCurrentSequence->GetSequenceId() == sequenceId)
-		{			
-			return;
-		}		
-
-		auto iter = mSequences.find(sequenceId);
-		assert(iter != mSequences.end());
-		mCurrentSequence = iter->second.get();
-		mTimer.Reset(mCurrentSequence->GetDuration());
-		mFrameIndex = 0;
-		UpdateFrame();
-	}
-
-	void SaveToFile(const std::string& filePath)
-	{
-		YAML::Emitter emitter;
-		Serialize(emitter);
-		std::ofstream file(filePath);
-		file << emitter.c_str();
-	}
-
-	void LoadFromFile(const std::string& filePath, ResourceLocator& locator)
-	{
-		YAML::Node node = YAML::LoadFile(filePath);
-		Deserialize(node, locator);
-	}
+	// IO
+	void SaveToFile(const std::string& filePath);
+	void LoadFromFile(const std::string& filePath, ResourceLocator& locator);
 
 	// ISerialize interface
-	void Serialize(YAML::Emitter& emitter) override
-	{
-		emitter << YAML::BeginSeq;
-		for (const auto& sequence : mSequences)
-		{
-			emitter << YAML::BeginMap;
-			emitter << YAML::Key << "key" << YAML::Value << sequence.first;
-			emitter << YAML::Key << "framesPerSecond" << YAML::Value << sequence.second->GetFramesPerSecond();
-			emitter << YAML::Key << "frames" << YAML::Value;
-			sequence.second->Serialize(emitter);
-			emitter << YAML::EndMap;
-		}
-		emitter << YAML::EndSeq;
-	}
-
+	void Serialize(YAML::Emitter& emitter) override;
 	void Deserialize(const YAML::Node& node, ResourceLocator& locator) override;
 
 private:
-	SequenceFrame& GetFrame()
-	{
-		assert(mCurrentSequence != nullptr);
-		mCurrentSequence->GetFrame(mOutSequenceFrame, mFrameIndex);
-		return mOutSequenceFrame;
-	}
-
-	void Initialize(std::string sequenceId)
-	{
-		SetAnimationSequence(sequenceId);
-		auto& frame = GetFrame();
-		mSprite = std::make_unique<sf::Sprite>(*frame.mTexture);
-		mSprite->setOrigin(frame.GetOrigin(mOriginAnchor));
-	}
-
-	void UpdateFrame()
-	{
-		if (mSprite != nullptr)
-		{
-			auto& frame = GetFrame();
-			mSprite->setTexture(*frame.mTexture);
-			mSprite->setTextureRect(frame.mTextureRect);
-			mSprite->setOrigin(frame.GetOrigin(mOriginAnchor));
-		}
-	}
+	void RefreshFrame();
+	SequenceFrame& GetFrame();
 
 private:
 	// sequence memebers
-	std::map<std::string, std::shared_ptr<AnimationSequence>> mSequences;
+	std::unordered_map<std::string, std::shared_ptr<AnimationSequence>> mSequences;
 	AnimationSequence* mCurrentSequence{ nullptr };
 	SequenceFrame mOutSequenceFrame;	
 
