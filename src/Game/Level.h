fb0fdc4fa986974986262a03f6dc56cf5bb59e1d
twje <11610634+twje@users.noreply.github.com>
2023-10-01 23:54:10 +1100

    Track player with camera

--- src/Game/Level.h
+++ src/Game/Level.h
@@ -5,12 +5,23 @@
 #include "Player.h"
 #include "Overlay.h"
 
+#include <iostream>
 class Level : public Scene
 {
 public:
 	void Create() override
 	{
+		const ApplicationConfig& config = GetResourceLocator().GetApplicationConfig();
+		sf::Vector2f windowSize = sf::Vector2f(config.GetWindowSize());
+
+		mWorldView.setSize(windowSize);
+		mWorldView.setCenter(windowSize * 0.5f);
+
+		mHUDView.setSize(windowSize);
+		mHUDView.setCenter(windowSize * 0.5f);
+
 		mPlayer = CreateGameObject<Player>(GetResourceLocator(), sf::Vector2f(0, 0));
+
 		mAllSprites.Add(mPlayer);
 
 		mOverlay = std::make_unique<Overlay>(GetResourceLocator(), *mPlayer);
@@ -28,17 +39,30 @@ public:
 
 	virtual void Draw(sf::RenderWindow& window)
 	{
+		mWorldView.setCenter(mPlayer->GetCenter());
+		window.setView(mWorldView);
+
 		for (GameObject* gameObject : mAllSprites)
 		{
 			if (!gameObject->IsMarkedForRemoval()) { continue; }
 
 			window.draw(*gameObject);
 		}
+
+		window.setView(mHUDView);
 		mOverlay->Draw(window);
+	}
+
+	virtual void OnWindowResize(const sf::Vector2u& size)
+	{
+		mWorldView.setSize(sf::Vector2f(size));
+		mHUDView.setSize(sf::Vector2f(size));
 	}
 
 private:
 	Player* mPlayer;
 	Group mAllSprites;
 	std::unique_ptr<Overlay> mOverlay;
+	sf::View mWorldView;
+	sf::View mHUDView;
 };
