2c67d55b7f2b5a48a93289a0772624613d4fdc75
Thomas Edwards <11610634+twje@users.noreply.github.com>
2023-12-09 14:38:19 +1100

    Factored out draw region

--- vendor/Core/include/Core/Tiled/TiledMapAsset.h
+++ vendor/Core/include/Core/Tiled/TiledMapAsset.h
@@ -157,6 +157,46 @@ private:
 };
 
 //------------------------------------------------------------------------------
+class TiledMapViewRegion
+{
+public:
+	TiledMapViewRegion(const TiledMapData& data, const sf::IntRect& screenViewRegion)
+	{
+		uint32_t tileWidth = data.GetTileWidth();
+		uint32_t tileHeight = data.GetTileHeight();
+		uint32_t mapWidth = data.GetMapWidth();
+		uint32_t mapHeight = data.GetMapHeight();
+
+		uint32_t left = screenViewRegion.left;
+		uint32_t top = screenViewRegion.top;
+		uint32_t right = screenViewRegion.left + screenViewRegion.width;
+		uint32_t bottom = screenViewRegion.top + screenViewRegion.height;
+
+		mStartX = Clamp(0, mapWidth, left) / tileWidth;
+		mStartY = Clamp(0, mapHeight, top) / tileHeight;
+		mEndX = std::ceil(static_cast<float>(Clamp(0, mapWidth, right)) / tileWidth);
+		mEndY = std::ceil(static_cast<float>(Clamp(0, mapHeight, bottom)) / tileHeight);
+	}
+
+	size_t GetStartX() const { return mStartX; }
+	size_t GetStartY() const { return mStartY; }
+	size_t GetEndX() const { return mEndX; }
+	size_t GetEndY() const { return mEndY; }
+
+private:
+	int32_t Clamp(int32_t minValue, int32_t maxValue, int32_t value)
+	{
+		return std::min(maxValue, std::max(minValue, value));
+	}
+
+private:
+	size_t mStartX;
+	size_t mStartY;
+	size_t mEndX;
+	size_t mEndY;
+};
+
+//------------------------------------------------------------------------------
 class TiledMapAsset : public Asset, private NonCopyableNonMovableMarker
 {
 public:
@@ -164,11 +204,13 @@ public:
 		: mData(std::move(data))
 	{ }
 
-	void Draw(sf::RenderWindow& window, const sf::IntRect& region)
+	void Draw(sf::RenderWindow& window, const sf::IntRect& screenViewRegion)
 	{
+		TiledMapViewRegion viewRegion(*mData.get(), screenViewRegion);
+		
 		for (const TiledLayer& layer : mData->GetTiledLayers())
 		{
-			DrawLayer(window, layer, region);
+			DrawLayer(window, layer, viewRegion);
 		}
 	}
 
@@ -196,11 +238,6 @@ public:
 		return descriptors;
 	}
 
-	//const SpritesheetTiledSet& GetSpritesheetTiledSet(uint32_t globalTileId) const
-	//{
-	//	return mData->GetSpritesheetTiledSet(globalTileId);
-	//}
-
 	const TextureRegion& GetTextureRegion(uint32_t firstGid, uint32_t globalTileId)
 	{
 		const TileTextureLookup* tileTextureLookup = mTileTextureLookupMap.at(firstGid).get();
@@ -209,21 +246,14 @@ public:
 	}
 
 private:
-	void DrawLayer(sf::RenderWindow& window, const TiledLayer& layer, const sf::IntRect& region)
-	{	
+	void DrawLayer(sf::RenderWindow& window, const TiledLayer& layer, const TiledMapViewRegion& viewRegion)
+	{
 		uint32_t tileWidth = mData->GetTileWidth();
 		uint32_t tileHeight = mData->GetTileHeight();
-		uint32_t mapWidth = mData->GetMapWidth();
-		uint32_t mapHeight = mData->GetMapHeight();
 
-		size_t startX = Clamp(0, mapWidth, region.left) / tileWidth;
-		size_t startY = Clamp(0, mapHeight, region.top) / tileHeight;
-		size_t endX = std::ceil(static_cast<float>(Clamp(0, mapWidth, region.left + region.width)) / tileWidth);
-		size_t endY = std::ceil(static_cast<float>(Clamp(0, mapHeight, region.top + region.height)) / tileHeight);
-
-		for (size_t y = startY; y < endY; y++)
+		for (size_t y = viewRegion.GetStartY(); y < viewRegion.GetEndY(); y++)
 		{
-			for (size_t x = startX; x < endX; x++)
+			for (size_t x = viewRegion.GetStartX(); x < viewRegion.GetEndX(); x++)
 			{
 				DrawTile(window, layer, x, y, tileWidth, tileHeight);
 			}
