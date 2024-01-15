0c5e92197e47360a03b704f2e3f33d8a0458871f
Thomas Edwards <11610634+twje@users.noreply.github.com>
2024-01-15 21:09:16 +1100

    Auto tiling

--- src/SoilLayer.h
+++ src/SoilLayer.h
@@ -68,25 +68,62 @@ private:
             gameObject->Kill();
         }
 
-        AssetManager& assetManager = ResourceLocator::GetInstance().GetAssetManager();
-        sf::Texture& texture = assetManager.GetAsset<Texture>("o").GetRawTexture();                
-        sf::IntRect textureRegion(sf::Vector2i(), sf::Vector2i(texture.getSize()));
-
-        for (const SoilCell& soilTile : mGrid)
+        for (const SoilCell& tile : mGrid)
         {
-            if (soilTile.mIsHit)
+            if (tile.mIsHit)
             {
-                float posX = soilTile.mTileIndex.x * mMap->GetTileSize().x;
-                float posY = soilTile.mTileIndex.y * mMap->GetTileSize().y;
+                bool t = mGrid.at(TileIndex(tile.mTileIndex.x, tile.mTileIndex.y - 1)).mIsHit;
+                bool b = mGrid.at(TileIndex(tile.mTileIndex.x, tile.mTileIndex.y + 1)).mIsHit;
+                bool r = mGrid.at(TileIndex(tile.mTileIndex.x + 1, tile.mTileIndex.y)).mIsHit;
+                bool l = mGrid.at(TileIndex(tile.mTileIndex.x - 1, tile.mTileIndex.y)).mIsHit;
 
-                auto sprite =  mScene.CreateGameObject<Generic>(texture,
-                                                                textureRegion,
-                                                                sf::Vector2f(),
-                                                                sf::Vector2f(posX, posY));
-                mSoilSprites.Add(sprite);
-                mAllSprites.Add(sprite);
+                std::string tileType = "o";
+
+                // all sides
+                if (t && b && r && l) { tileType = "x"; }
+
+                // horizontal tiles only
+                if (l && !(t || r || b)) { tileType = "r"; }
+                if (r && !(t || l || b)) { tileType = "l"; }
+                if (r && l && !(t || b)) { tileType = "lr"; }
+
+                // vertical tiles only
+                if (t && !(r || l || b)) { tileType = "b"; }
+                if (b && !(r || l || t)) { tileType = "t"; }
+                if (b && t && !(r || l)) { tileType = "tb"; }
+
+                // corners
+                if (l && b && !(t || r)) { tileType = "tr"; }
+                if (r && b && !(t || l)) { tileType = "tl"; }
+                if (l && t && !(b || r)) { tileType = "br"; }
+                if (r && t && !(b || l)) { tileType = "bl"; }
+
+                // T shapes
+                if ((t && b && r) && !l) { tileType = "tbr"; }
+                if ((t && b && l) && !r) { tileType = "tbl"; }
+                if ((l && r && t) && !b) { tileType = "lrb"; }
+                if ((l && r && b) && !t) { tileType = "lrt"; }
+
+                AddSoilTile(tileType, tile.mTileIndex);
             }
         }
+    }
+
+    void AddSoilTile(const std::string& tileType, sf::Vector2i tileIndex)
+    {
+        AssetManager& assetManager = ResourceLocator::GetInstance().GetAssetManager();
+        sf::Texture& texture = assetManager.GetAsset<Texture>(tileType).GetRawTexture();
+        sf::IntRect textureRegion(sf::Vector2i(), sf::Vector2i(texture.getSize()));
+        
+        float posX = tileIndex.x * mMap->GetTileSize().x;
+        float posY = tileIndex.y * mMap->GetTileSize().y;
+
+        auto sprite = mScene.CreateGameObject<Generic>(texture,
+                                                       textureRegion,
+                                                       sf::Vector2f(),
+                                                       sf::Vector2f(posX, posY));
+        mSoilSprites.Add(sprite);
+        mAllSprites.Add(sprite);
     }
 
     size_t TileIndex(size_t x, size_t y)
